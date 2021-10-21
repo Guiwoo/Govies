30949293e1139f59435736ae07f793dcb9637aab
Guiwoo <park.guiwoo@hotmail.com>
2021-10-21 18:28:25 +0900

    Loading

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import React from "react";
+import * as Font from "expo-font";
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
+import { Image } from "react-native";
+
+// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+// const loadImages = (images) =>
+//   images.map((image) => {
+//     if (typeof image === "string") {
+//       return Image.prefetch(image);
+//       //on the web await Image.petch()
+//     }
+//     return Asset.loadAsync(image);
+//   });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [assets] = useAssets([require("./assets/somiFace.jpg")]);
+  const [loaded] = Font.useFonts([Ionicons.font]);
+  if (!assets || !loaded) {
+    return <AppLoading />;
+  }
+  return null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
